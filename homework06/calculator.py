305d74fb2888d32be7846e7a09b8e38d45fc2137
Indy <spenceraj11@gmail.com>
2024-11-07 18:43:08 -0700

    blab

---
+++ homework06/calculator.py
@@ -0,0 +1,23 @@
+from pair import Pair, nil
+
+
+def tokenize(expression):
+    return expression.replace("(","( ").replace(")"," )").split()
+
+def parse_tokens(tokens, index):
+    """ my doctests
+    >>> parse_tokens(tokenize('(* (- 8 4) 4)') , 0)
+    Pair('*', Pair(Pair('-', Pair(8, Pair(4, nil))), Pair(4, nil)))
+    """
+    if cond 1
+        
+    elif cond 2
+        
+    else:
+        try:
+
+
+
+        except:
+            raise TypeError
+    pass
